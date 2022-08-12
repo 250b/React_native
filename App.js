b5e3be2d0dbfdc12c38f2b4c35cde02e8a661929
BORI <bohhh_@naver.com>
2022-08-12 15:13:11 +0900

    Login-screen

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {useFonts,
+        NotoSansKR_100Thin, NotoSansKR_300Light,
+        NotoSansKR_400Regular, NotoSansKR_500Medium,
+        NotoSansKR_700Bold, NotoSansKR_900Black} from '@expo-google-fonts/noto-sans-kr';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import {LoginScreen} from "./src/screens/LoginScreen";
+import {SignUpScreen} from "./src/screens/SignUpScreen";
+import {HomeScreen} from "./src/screens/HomeScreen";
+import {MapScreen} from "./src/screens/MapScreen"
+import {RecordScreen} from "./src/screens/RecordScreen";
+import {CommunityScreen} from "./src/screens/CommunityScreen";
+import {SettingScreen} from "./src/screens/SettingScreen";
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    NotoSansKR_100Thin, NotoSansKR_300Light, NotoSansKR_400Regular,
+    NotoSansKR_500Medium, NotoSansKR_700Bold, NotoSansKR_900Black
+  });
+
+  if (!fontsLoaded) return null;
+  return <SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Map" component={MapScreen}/>
+        <Tab.Screen name="Record" component={RecordScreen}/>
+        <Tab.Screen name="Community" component={CommunityScreen}/>
+        <Tab.Screen name="Setting" component={SettingScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  </SafeAreaProvider>
+}
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,88 @@
+import { StyleSheet, Pressable, Text, View } from 'react-native';
+import {useState} from 'react';
+import LoginInput from '../login-signUp-component/LoginInput';
+import BottomButton from '../login-signUp-component/BottomButton';
+import 'react-native-gesture-handler';
+
+export const LoginScreen=({navigation})=>{
+    const[id, setId] =useState('');
+    const [pw, setPw] = useState('');
+
+    const idHandler=(text)=>{
+        setId(text)
+    }
+
+    const pwHandler=(text)=>{
+        setPw(text)
+    }
+
+    return (
+        <View style={styles.contianer}>
+            <View style={styles.image}></View>{/*나중에 image로 교체*/}
+            <Text style={styles.title}>SAVEARTH</Text>
+
+              <LoginInput
+                  name={"아이디"}
+                  value={id}
+                  setValue={idHandler}
+                  placeholder={"아이디를 입력해주세요"}/>
+              <LoginInput
+                  name={"비밀번호"}
+                  value={pw}
+                  setValue={pwHandler}
+                  placeholder={"비밀번호를 입력해주세요"}
+                  secureTextEntry={true}/>
+            
+            <View style={styles.signUpTextContainer}>
+              <Text style={styles.signUpText}>아직 회원이 아니신가요?</Text>
+              <Pressable onPress={()=>navigation.navigate('SignUp')}><Text style={[styles.signUpText, styles.signUpTextLink]}> 회원가입</Text></Pressable>
+            </View>
+          <BottomButton style={styles.loginButton } text={"로그인"} onPress={()=>navigation.navigate('Home')}/>
+        </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    contianer: {
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center'
+
+    },
+    image:{
+      width:105,
+      height:105,
+      borderColor: 'black',
+      borderWidth:2,
+      borderRadius: 20,
+      marginBottom: 10,
+      marginTop: 134
+    },
+    title:{
+      fontWeight: 'bold',
+      fontSize: 34,
+      marginBottom:51,
+      fontFamily: 'NotoSansKR_700Bold',
+      color: '#E0E0E0',
+      height: 41
+
+    },
+    signUpTextContainer:{
+      flexDirection:'row',
+      marginTop:24,
+      marginBottom: 59,
+      height: 22,
+    },
+    signUpText:{
+      fontSize:13,
+      fontFamily: 'NotoSansKR_400Regular',
+      color:'#797979',
+    },
+    signUpTextLink:{
+      textDecorationLine: 'underline'
+    },
+    loginButton:{
+      justifyContent: 'flex-end'
+    }
+  });
